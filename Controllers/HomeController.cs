263c24653f8a6851c99eb437ffff9770224b7b17
Jake Humphreys <h024470h@student.staffs.ac.uk>
2021-01-03 17:20:40 +0000

    UI changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EIRLSSAssignment1.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +10,19 @@ namespace EIRLSSAssignment1.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext _appDbContext;
+
+        public HomeController()
+        {
+            _appDbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
+            ApplicationUser user = _appDbContext.Users.Find(User.Identity.GetUserId());
+            ViewBag.User = user;
+            ViewBag.FirstName = user.Name.Substring(0, user.Name.IndexOf(" "));
             return View();
         }
 
